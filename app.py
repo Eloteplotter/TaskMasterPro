c26690ac1903610b578b85e390011c19b76668d6
Eloteplotter <joelgonzalezmoraga19@gmail.com>
2023-11-30 16:58:30 -0600

    hola

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ load_dotenv()
 
 app =Flask(__name__)
 
+
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
+app.config['SECRET_KEY'] = 'super secret key'
 app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 def login_required(f):
@@ -34,84 +37,114 @@ db = SQL("sqlite:///block.db")
 def registro():
     
     if request.method == "POST":
-         username = request.form.get("username")
-         email = request.form.get("email")
-         password = request.form.get("password")
-         confirmation = request.form.get("confirmation")
-         #confirma la contrase;a 
-         if password != confirmation:
-             return "hola"
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        #confirma la contrase;a 
+        if password != confirmation:
+            return "algo"
         
-         #se agrega el nuevo usario
-         db.execute('INSERT INTO users username, email, password VALUES (?, ?, ?)', username, email, generate_password_hash(password))
-         return redirect ("login.html")
+        row = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+        if len(row) != 0:
+            return "eso significa que hay otro usuario con el mismo username"
+        
+        else:
+            #se agrega el nuevo usario
+            db.execute('INSERT INTO users(username, email, password) VALUES (?, ?, ?)', username, email, password)
+            return redirect("/login")
     else:
-         return redirect ("registro.html")
+         return render_template("registro.html")
         
+
+
+@app.route("/login", methods=["GET", "POST"])
+def log_ing():
+
+    session.clear()
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+       
+
+        if not username or not password:
+            pass #por el momentoo, idea que ponga un mensaje en rojo que no coinciden las contra o algo asi
+
+        rows = db.execute('SELECT * FROM users WHERE username = (?)', username)
+        print(rows)
+
+        if  check_password_hash(rows[0]["password"], request.form.get("password")):
+            return "aqui un mensaje donde diga la contra o el username es erroneo"
+
+        session["user_id"] = rows[0]["id"] 
+
+        return redirect("/")
 
 
 @app.route("/", methods=["GET", "POST"])
-def log_ing():
-
-     session.clear()
-
-     if request.method == "GET":
-         return render_template("login.html")
-
-     else:
-         name = request.form.get("name")
-         password = request.form.get("password")
-         user_id = session["user_id"]
-
-         if not name or not password:
-             pass #por el momentoo, idea que ponga un mensaje en rojo que no coinciden las contra, supongo que que js 
-
-         session["user_id"] = rows[0]["id"] 
-
-         rows = db.execute('SELECT name, password FROM users WHERE name = (?)', name)
-
-         if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
-             return #aqui un mensaje donde diga la contra o el username es erroneo 
-
-     return redirect("/")
-
-
-@app.route("/index", methods=["GET", "POST"])
+@login_required
 def index():
-    if not session.get("id"):
-        return redirect("/login")
-    
-    blocks = db.execute("SElECT * FROM block WHERE id_users = ? GROUP BY titulo" ,session["id"])
+ 
+    user_id = session.get("user_id")
+    blocks = db.execute("SELECT * FROM block WHERE user_id = ?", user_id)
+
     return render_template("index.html" , blocks = blocks)
 
-@app.route("/edit", methods=["POST"])
-def edit():
-   
-     title = request.form.get("titulo")
-     nota = request.form.get("nota")
-     start_time = request.form.get("star_time")
-     finish_time = request.form.get("finish_time")
 
-     db.execute("INSERT INTO block(titulo, descripcion, user_id, tiempo_inicio, tiempo_final) VALUES(?, ?, ?, ?, ?)", 
-                title, nota, session["id"], start_time, finish_time)
+
+@app.route("/edit", methods=["POST"])
+@login_required
+def edit():
     
-     return redirect("/")
+    title = request.form.get("editTitle")
+    descripcion = request.form.get("editDescripcion")
+    tiempo_inicio = request.form.get("editTiempo_inicio")
+    tiempo_final = request.form.get("editTiempo_final")
+    id = request.form.get("id")
+    
+    print(title, descripcion, tiempo_inicio, tiempo_final, id)
+
+    db.execute("UPDATE block SET titulo = ?, descripcion = ?, tiempo_inicio = ?, tiempo_final = ? WHERE id = ?", 
+                title, descripcion, tiempo_inicio, tiempo_final, id)
+    
+    return redirect("/")
+
+
+
+@app.route("/add", methods=["POST"])
+@login_required
+def add():
+    user_id = session.get("user_id")
+    title = request.form.get("titulo")
+    descripcion = request.form.get("descripcion")
+    tiempo_inicio = request.form.get("tiempo_inicio")
+    tiempo_final = request.form.get("tiempo_final")
+
+    db.execute('INSERT INTO block (titulo, descripcion, user_id, tiempo_inicio, tiempo_final) VALUES (?, ?, ?, ?, ?)',
+               title, descripcion, user_id, tiempo_inicio, tiempo_final,)
+   
+    return redirect("/")
+    
 
 
 @app.route("/delete", methods=["POST"])
+@login_required
 def delete():
-   
-     id = request.form.get("id_nota")
-     db.execute('DELETE * FROM block WHERE id = ?', id)
+    
+
+    id_nota = request.form.get("id_nota")
+    db.execute('DELETE FROM block WHERE id = ?', id_nota)
+
+    return redirect("/")
 
 @app.route("/logout")
+@login_required
 def logout():
 
      session.clear()
      return redirect("/")
-
-
-
-@app.route("/login", methods= ["GET", "POST"])
-def login():
-    return render_template("login.html")
